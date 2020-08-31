7235963e9251f2df57827424a047e5ee0855b6f0
matvki <m.vecchi@it-students.fr>
2020-08-31 09:01:36 +0200

    base
    base of the page

---
+++ js/script.js
@@ -0,0 +1,89 @@
+document.querySelector('#vues').addEventListener('mouseenter', () => {
+    document.querySelector("#txtVues").classList.remove('d-none');
+    document.querySelector("#imgVues").classList.add('bright');
+})
+document.querySelector('#vues').addEventListener('mouseleave', () => {
+    document.querySelector("#txtVues").classList.add('d-none');
+    document.querySelector("#imgVues").classList.remove('bright');
+})
+
+document.querySelector('#dvd').addEventListener('mouseenter', () => {
+    document.querySelector("#txtDvd").classList.remove('d-none');
+    document.querySelector("#imgDvd").classList.add('bright');
+})
+document.querySelector('#dvd').addEventListener('mouseleave', () => {
+    document.querySelector("#txtDvd").classList.add('d-none');
+    document.querySelector("#imgDvd").classList.remove('bright');
+})
+
+document.querySelector('#mep').addEventListener('mouseenter', () => {
+    document.querySelector("#txtMep").classList.remove('d-none');
+    document.querySelector("#imgMep").classList.add('bright');
+})
+document.querySelector('#mep').addEventListener('mouseleave', () => {
+    document.querySelector("#txtMep").classList.add('d-none');
+    document.querySelector("#imgMep").classList.remove('bright');
+})
+
+document.querySelector('#alb').addEventListener('mouseenter', () => {
+    document.querySelector("#txtAlb").classList.remove('d-none');
+    document.querySelector("#imgAlb").classList.add('bright');
+})
+document.querySelector('#alb').addEventListener('mouseleave', () => {
+    document.querySelector("#txtAlb").classList.add('d-none');
+    document.querySelector("#imgAlb").classList.remove('bright');
+})
+
+document.querySelector('#riso').addEventListener('mouseenter', () => {
+    document.querySelector("#txtRiso").classList.remove('d-none');
+    document.querySelector("#imgRiso").classList.add('bright');
+})
+document.querySelector('#riso').addEventListener('mouseleave', () => {
+    document.querySelector("#txtRiso").classList.add('d-none');
+    document.querySelector("#imgRiso").classList.remove('bright');
+})
+
+document.querySelector('#stage').addEventListener('mouseenter', () => {
+    document.querySelector("#txtStage").classList.remove('d-none');
+    document.querySelector("#imgStage").classList.add('bright');
+})
+document.querySelector('#stage').addEventListener('mouseleave', () => {
+    document.querySelector("#txtStage").classList.add('d-none');
+    document.querySelector("#imgStage").classList.remove('bright');
+})
+
+document.querySelector('#morph').addEventListener('mouseenter', () => {
+    document.querySelector("#txtMorph").classList.remove('d-none');
+    document.querySelector("#imgMorph").classList.add('bright');
+})
+document.querySelector('#morph').addEventListener('mouseleave', () => {
+    document.querySelector("#txtMorph").classList.add('d-none');
+    document.querySelector("#imgMorph").classList.remove('bright');
+})
+
+document.querySelector('#work').addEventListener('mouseenter', () => {
+    document.querySelector("#txtWork").classList.remove('d-none');
+    document.querySelector("#imgWork").classList.add('bright');
+})
+document.querySelector('#work').addEventListener('mouseleave', () => {
+    document.querySelector("#txtWork").classList.add('d-none');
+    document.querySelector("#imgWork").classList.remove('bright');
+})
+
+document.querySelector('#exp').addEventListener('mouseenter', () => {
+    document.querySelector("#txtExp").classList.remove('d-none');
+    document.querySelector("#imgExp").classList.add('bright');
+})
+document.querySelector('#exp').addEventListener('mouseleave', () => {
+    document.querySelector("#txtExp").classList.add('d-none');
+    document.querySelector("#imgExp").classList.remove('bright');
+})
+
+document.querySelector('#work2').addEventListener('mouseenter', () => {
+    document.querySelector("#txtWork2").classList.remove('d-none');
+    document.querySelector("#imgWork2").classList.add('bright');
+})
+document.querySelector('#work2').addEventListener('mouseleave', () => {
+    document.querySelector("#txtWork2").classList.add('d-none');
+    document.querySelector("#imgWork2").classList.remove('bright');
+})
