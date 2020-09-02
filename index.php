bfdd1bba2d648dbfe2b5722b247da70716d4a062
matvki <m.vecchi@it-students.fr>
2020-09-02 10:39:52 +0200

    update
    update headband

--- index.php
+++ index.php
@@ -11,8 +11,15 @@
     <header>
         <div id="headband">
             <div id="txt">
-                <img src="./img/logo-tevissen-7.png" alt="">
-                <h1 id="day">Bonjour !</h1>
+                <h1>Lucas <br> Tevissen</h1>
+                <div id="boxA">
+                    <?php include_once './controller/projetsCaller.php';
+                foreach ($projects as $key => $value) { ?>
+                    <a class="<?= $value['class'] ?>" href="#<?= $value['class'] ?>"><?php echo ucfirst($value['name']) ?></a><br>
+                <?php } ?>
+                </div>
+                
+                <h2 id="day">Bonjour !</h2>
                 <p id="lulu">Je suis étudiant en troisième année de Design Graphique. Passionné, je ne demande qu’à partager. <br>La création, l’art, les nouvelles technologies et l’innovation.</p>
                 <p>Langue</p>
                 <div id="language">
@@ -28,9 +35,8 @@
         </div>
     </header>
     <main>
-        <?php include_once './controller/projetsCaller.php';
-        foreach ($projects as $key => $value) { ?>
-            <div class="pic">
+        <?php foreach ($projects as $key => $value) { ?>
+            <div class="pic" id="<?= $value['class'] ?>">
                 <img src="./img/<?= $value['picture'] ?>" alt="<?= $value['name'] ?>">
                 <div class="square">
                     <h1><?= $value['name'] ?></h1>
@@ -40,6 +46,7 @@
     </main>
     <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.6/lib/darkmode-js.min.js"></script>
     <script src="./js/nightMode.js"></script>
+    <script src="./js/script.js"></script>
     <script src="./js/scroll.js"></script>
 </body>
 
---
+++ js/script.js
@@ -0,0 +1,48 @@
+document.querySelector('.branding').addEventListener('click', () => {
+    document.querySelector(".branding").classList.add('clickA');
+    document.querySelector(".vues").classList.remove('clickA');
+    document.querySelector(".jaquette").classList.remove('clickA');
+    document.querySelector(".typo").classList.remove('clickA');
+    document.querySelector(".motif").classList.remove('clickA');
+    document.querySelector(".mais").classList.remove('clickA');
+})
+document.querySelector(".vues").addEventListener('click', () => {
+    document.querySelector(".vues").classList.add('clickA');
+    document.querySelector(".branding").classList.remove('clickA');
+    document.querySelector(".jaquette").classList.remove('clickA');
+    document.querySelector(".typo").classList.remove('clickA');
+    document.querySelector(".motif").classList.remove('clickA');
+    document.querySelector(".mais").classList.remove('clickA');
+})
+document.querySelector('.jaquette').addEventListener('click', () => {
+    document.querySelector(".jaquette").classList.add('clickA');
+    document.querySelector(".vues").classList.remove('clickA');
+    document.querySelector(".branding").classList.remove('clickA');
+    document.querySelector(".typo").classList.remove('clickA');
+    document.querySelector(".motif").classList.remove('clickA');
+    document.querySelector(".mais").classList.remove('clickA');
+})
+document.querySelector('.typo').addEventListener('click', () => {
+    document.querySelector(".typo").classList.add('clickA');
+    document.querySelector(".vues").classList.remove('clickA');
+    document.querySelector(".jaquette").classList.remove('clickA');
+    document.querySelector(".branding").classList.remove('clickA');
+    document.querySelector(".motif").classList.remove('clickA');
+    document.querySelector(".mais").classList.remove('clickA');
+})
+document.querySelector('.motif').addEventListener('click', () => {
+    document.querySelector(".motif").classList.add('clickA');
+    document.querySelector(".vues").classList.remove('clickA');
+    document.querySelector(".jaquette").classList.remove('clickA');
+    document.querySelector(".typo").classList.remove('clickA');
+    document.querySelector(".branding").classList.remove('clickA');
+    document.querySelector(".mais").classList.remove('clickA');
+})
+document.querySelector('.mais').addEventListener('click', () => {
+    document.querySelector(".mais").classList.add('clickA');
+    document.querySelector(".vues").classList.remove('clickA');
+    document.querySelector(".jaquette").classList.remove('clickA');
+    document.querySelector(".typo").classList.remove('clickA');
+    document.querySelector(".motif").classList.remove('clickA');
+    document.querySelector(".branding").classList.remove('clickA');
+})
