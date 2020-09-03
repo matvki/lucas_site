bb52aa5011afc92726050a033f1e25ae5b1f40a1
matvki <m.vecchi@it-students.fr>
2020-09-03 14:58:49 +0200

    update for projet.php

--- controller/takeId.php
+++ controller/takeId.php
@@ -1,3 +1,26 @@
 <?php
+session_start();
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    $user = "root";
+    $password = "0000";
+    $bdd = new PDO("mysql:host=localhost;dbname=siteLuca;charset=utf8", $user, $password);
+    $request = $bdd->prepare("SELECT * FROM text WHERE projet like :id");
+    $request->bindParam(':id', $id);
+    $request->execute();
+    $txtProjet = $request->fetchall(PDO::FETCH_ASSOC);
+    $_SESSION['txtProjet'] = $txtProjet;
 
-var_dump($_POST);
+    $bdd = new PDO("mysql:host=localhost;dbname=siteLuca;charset=utf8", $user, $password);
+    $request = $bdd->prepare("SELECT * FROM image WHERE projet like :id");
+    $request->bindParam(':id', $id);
+    $request->execute();
+    $imgProjet = $request->fetchall(PDO::FETCH_ASSOC);
+    $_SESSION['imgProjet'] = $imgProjet;
+    
+    header("Location: ../project.php");
+    die;
+}else{
+    header("Location: ../");
+    die;
+}
